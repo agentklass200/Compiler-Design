9a48777475e995c6d601c43c348926ee3cb08a38
Alecxis Banag <alecxisbanag@hotmail.com>
2016-02-29 00:41:41 +0800

    Add comment

--- Compiler.java
+++ Compiler.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 import LexicalAnalyzer.Scanner;
 
+
+// Main Class
 public class Compiler {
 	public static void main(String[] args) throws IOException {
 		String fileName = "text.txt";
