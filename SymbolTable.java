8a05443763e8186d4dc205b0009b75f58c1df0eb
mariano <mariano>
2016-03-05 18:06:18 +0800

    Supot ako

--- SymbolTable.java
+++ SymbolTable.java
@@ -17,8 +17,20 @@ public class SymbolTable {
    
     public HashMap<String, Token> table = new HashMap<String, Token>();
 
-    public SymbolTable(){
-       table.put("Add",new Token("Add","+","Arithmetic Operator"));   
+    public SymbolTable(){                
+        table.put("Integer", new Token("Integer", "INT", "Data Type"));
+        table.put("Boolean", new Token("Boolean", "BOOL", "Data Type"));
+        table.put("String", new Token("String", "STR", "Data Type"));
+        table.put("Character", new Token("Character", "CHAR", "Data Type"));
+        table.put("Float", new Token("Float", "FLOAT", "Data Type"));
+        
+        table.put("Input", new Token("Input", "OBTAIN", "Input and Output"));
+        table.put("Output", new Token("Output", "REVEAL", "Input and Output"));
+        
+        table.put("If", new Token("If", "IF", "Conditional Statements"));        
+        table.put("ElseIf", new Token("ElseIf", "ELSEIF", "Conditional Statements"));
+        table.put("Else", new Token("Else", "ELSE", "Conditional Statements"));
+        table.put("EndIf", new Token("EndIf", "ENDIF", "Conditional Statements"));                                                
     }
         
 }
