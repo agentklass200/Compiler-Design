c5a8a363f44631de63e07b082b5913deb9cbd617
Alecxis Banag <alecxisbanag@hotmail.com>
2016-03-05 16:09:39 +0800

    Define Token Class

--- LexicalAnalyzer/Token.java
+++ LexicalAnalyzer/Token.java
@@ -1,5 +1,42 @@
 package LexicalAnalyzer;
 
 public class Token {
+	private String name;
+	private String value;
+	private String lexeme;
+	
+	public Token(String name, String value){
+		this.name = name;
+		this.value = value;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getLexeme() {
+		return lexeme;
+	}
+
+	public void setLexeme(String lexeme) {
+		this.lexeme = lexeme;
+	}
+	
+	
+	
+	
 }
+
+
