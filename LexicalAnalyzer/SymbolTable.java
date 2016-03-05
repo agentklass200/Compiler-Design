7acd6ee9ffff0eb8b73a35fe70972c63b711a22a
mariano <mariano>
2016-03-05 18:14:23 +0800

    add table

--- LexicalAnalyzer/SymbolTable.java
+++ LexicalAnalyzer/SymbolTable.java
@@ -57,6 +57,19 @@ public class SymbolTable {
        table.put("TrueBoolean",new Token("TrueBoolean","TRUE","Constant"));
        table.put("FalseBoolean",new Token("FalseBoolean","FALSE","Constant"));
        
+       table.put("Integer", new Token("Integer", "INT", "Data Type"));
+        table.put("Boolean", new Token("Boolean", "BOOL", "Data Type"));
+        table.put("String", new Token("String", "STR", "Data Type"));
+        table.put("Character", new Token("Character", "CHAR", "Data Type"));
+        table.put("Float", new Token("Float", "FLOAT", "Data Type"));
+        
+        table.put("Input", new Token("Input", "OBTAIN", "Input and Output"));
+        table.put("Output", new Token("Output", "REVEAL", "Input and Output"));
+        
+        table.put("If", new Token("If", "IF", "Conditional Statements"));        
+        table.put("ElseIf", new Token("ElseIf", "ELSEIF", "Conditional Statements"));
+        table.put("Else", new Token("Else", "ELSE", "Conditional Statements"));
+        table.put("EndIf", new Token("EndIf", "ENDIF", "Conditional Statements"));                                                
        
     }
         
